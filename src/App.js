ea40fd1dc29554347c553e50f3a12ca7fb0e34c5
zuzanna_brzozowska <zuzabrzozowska@gmail.com>
2020-06-28 18:07:30 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav>
+        <div className="container">
+          <span>superhero app</span>
+          <form>
+            <input type="text"></input>
+            <button>find hero</button>
+          </form>
+        </div>
+      </nav>
+      <main>
+        <div className="container">
+
+        </div>
+      </main>
+
     </div>
   );
 }
