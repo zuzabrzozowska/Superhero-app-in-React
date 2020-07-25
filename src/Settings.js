d62db69d59e4906f64d1a8add7a14a4a2417953c
zuzanna_brzozowska <zuzabrzozowska@gmail.com>
2020-07-25 21:00:57 +0200

    added filter function in Settings, todo: move the filtering to SearchResults, make two functions out of the filterHeroes function as it needs 4 different params

--- src/Settings.js
+++ src/Settings.js
@@ -3,77 +3,83 @@ import { getHeroAppearence } from './requests';
 import { Link } from 'react-router-dom';
 
 function Nav() {
-    const [results, setResults] = useState([]);
-    const [errorText, setErrorText] = useState(''); 
-    const [allHeroesIDs, setAllHeroesIDs] = useState([]);
+  const [results, setResults] = useState([]); //niepotrzebne tu?
+  const [errorText, setErrorText] = useState(''); 
+  const [loading, setLoading] = useState(true); //niepotrzebne?
+  const [allHeroesIDs, setAllHeroesIDs] = useState([]); //niepotrzebne?
+  const [btnType, setBtnType] = useState('');
+  const [btnText, setBtnText] = useState('');
+    
+  const filterHeroes = event => {
+    setBtnType(event.target.dataset.type);
+    setBtnText(event.target.innerText);
 
-    const filterWomen = () => {
-      //get list with all the ids from 1 to 731
-      let ids = [];
-      for (let i = 1; i <= 10; i++) {
-        ids.push(i);
-        console.log(ids);
-      }
-      console.log(ids);
-      
-      /* 
+    //how to get this out of this function?
+    let ids = [];
+    for (let i = 1; i <= 10; i++) {
+      ids.push(i);
+    }
+    setAllHeroesIDs(ids);
+
+    let objectArray = [];
+
+    ids.forEach(id => {
       getHeroAppearence(id).then(data => {
         if (data.error) {
           setErrorText(data.error);
           return;
         }
-
-             
-        if (res) {
-          res = res.filter(item => {return (item.appearance.gender === "Female") });
-          setResults(res);
-          setErrorText('');
-        } 
-      })*/
-    } 
-
+        objectArray.push(data);
+        if (btnText !== 'All' && btnText !== 'Other') {
+          objectArray = objectArray.filter(item => {return (item[`${btnType}`] === `${btnText}`) });
+        } else if (btnText === 'Other') {
+          objectArray = objectArray.filter(item => 
+            { return (
+              item[`${btnType}`] !== 'Human' 
+              && item[`${btnType}`] !== 'Alien' 
+              && item[`${btnType}`] !== 'Mutant') 
+            });
+        }
+      }) 
+    })
+    setTimeout(() => {
+      setResults(objectArray);
+    }, 5000)
+    setLoading(false);
+  }
   
     return (
         <nav>
             <div className="container container__nav">
+              <h1 className="about-hero__title">Search Settings</h1>
 
-              <Link to="/" className="link link-homepage"><i className="icon-homepage fas fa-arrow-circle-left"></i></Link>
+                <label>I'm looking for</label>
+                <div className="settings__btn-box">
+                  <button data-type="gender" onClick={filterHeroes}>Female</button>
+                  <button data-type="gender" onClick={filterHeroes}>Male</button>
+                  <button data-type="gender" onClick={filterHeroes}>All</button>
+                </div>
+            
+                <label>Race</label>
+                <div className="settings__btn-box">
+                  <button data-type="race" onClick={filterHeroes}>Human</button>
+                  <button data-type="race" onClick={filterHeroes}>Alien</button>
+                  <button data-type="race" onClick={filterHeroes}>Mutant</button>
+                  <button data-type="race" onClick={filterHeroes}>Other</button>
+                  <button data-type="race" onClick={filterHeroes}>All</button>
+                </div>
+           
+                
+
+                <label>Height (cm)</label><input type="range" min="150" max="250" name="height"></input>
+
+                <Link to={`/search/${btnType}/${btnText}`}><button>Save</button></Link>
               
-              <label>I'm looking for</label><br/>
-              <button onClick={filterWomen}>Women</button><button>Men</button><button>All</button>
-
-              <br/>
-              <label>Race</label><br/>
-              <button>Human</button><button>Mutant</button><button>Other</button><button>All</button>
-              <br/>
-              <label>Height (cm)</label><br/>
-              <input></input>
-              <br/>
-              <button>Save</button>
-
             </div>
+
+            <Link to="/" className="link link-homepage"><i className="icon-homepage fas fa-arrow-circle-left"></i></Link>
         </nav>
     )
 }
 
 export default Nav;
-
-/*<form id="form">
-                <label>find by name</label>
-                <input onChange={updateHeroInput} type="text" value={inputValue} placeholder="wonder..." />
-                <Link className="link btn-search" to={`/search/${inputValue}`}>
-                  <button onClick={() => setResults([])}><i className="fas fa-search" /></button>
-                </Link>
-                <div className="results-box">
-                  { errorText && <p>{errorText}</p> }
-                  { !errorText && loading && <p>loading........</p>}
-                  { 
-                    results && inputValue && !loading &&
-                    <div>{results.map(item => 
-                      <Link className="link" to={`/${item.id}/${item.name}`}>
-                        <button onClick={() => setResults([])}>{item.name}</button>
-                      </Link>)}
-                    </div> 
-                  }
-                </div>
-                </form> */
